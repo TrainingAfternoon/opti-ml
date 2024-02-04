13bb624b45436e9c12f70c731638f255ea4e69db
Luke Harwood <student@localhost>
2024-02-04 20:35:15 +0000

    Check in work on arm server

---
+++ fuchsia_build_llvm.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+echo "Sourcing .env and venvpath"
+source .env
+source $VENVPATH/bin/activate
+
+# NOTE: MAKE SURE THAT LLVM IS CLONED TO LLVM_SRCDIR!
+
+# install dependencies
+echo "Installing LLVM dependencies"
+sudo apt-get install ninja-build lld
+
+# tensorflow stuff
+echo "Doing the tensorflow stuff"
+sudo apt-get install ccache gcc-multilib g++-multilib libxml2-dev automake libpthreadpool-dev
+TF_PIP=$(python3 -m pip show tensorflow | grep Location | cut -d ' ' -f 2)
+
+export TENSORFLOW_AOT_PATH="${TF_PIP}/tensorflow"
+export TFLITE_PATH=$HOME/tflite
+mkdir ${TFLITE_PATH}
+
+cd ${TFLITE_PATH}
+$OPTIML_SRCDIR/buildbot/build_tflite.sh
+
+# build the LLVM build scripts
+## RESOURCES:
+## https://llvm.org/docs/GettingStarted.html#hardware
+## https://llvm.org/docs/GettingStarted.html#getting-the-source-code-and-building-llvm
+## https://llvm.org/docs/CMake.html#cmake-build-type
+echo "Building LLVM"
+cd $LLVM_SRCDIR
+mkdir -p build
+cd build
+cmake \
+	-G "Ninja" \
+	-S $LLVM_SRCDIR/llvm \
+	-DLLVM_ENABLE_PROJECTS='clang' \
+	-DCMAKE_INSTALL_PREFIX='~/workspace/llvm-install' \
+	-DCMAKE_BUILD_TYPE='Debug' \
+	-DLLVM_USE_LINKER=lld \
+	-C ${TFLITE_PATH}/tflite.cmake
+ninja check-llvm clang
+
+echo "Done"
+deactivate
---
+++ install_fuchsia.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# https://fuchsia.dev/fuchsia-src/get-started/get_fuchsia_source
+
+source .env
+
+# NOTE: preflight only works on x64 so we skip it
+
+# run bootstrap script
+cd $HOME
+curl -s "https://fuchsia.googlesource.com/fuchsia/+/HEAD/scripts/bootstrap?format=TEXT" | base64 --decode | bash
+
+# set up profile with necessary envars
+echo "export PATH=$PATH:$HOME/fuchsia/.jiri_root/bin" >> $HOME/.profile
+echo "source $HOME/fuchsia/scripts/fx-env.sh" >> $HOME/.profile
+
+# install IDK
+cipd install fuchsia/sdk/core/linux-arm64 latest -root ${IDK_DIR}
+
+# install sysroot
+cipd install fuchsia/sysroot/linux-arm64 latest -root ${SYSROOT_DIR}/linux-arm64
+#cipd install fuchsia/sysroot/linux-amd64 latest -root ${SYSROOT_DIR}/linux-x64
