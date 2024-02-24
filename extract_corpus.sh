06a4713d330efe7924d4fad1a09d84d2d34abfa4
Sam Keyser <keysers@msoe.edu>
2024-02-24 22:36:55 +0000

    Crispy doom build scripts

--- extract_corpus.sh
+++ extract_corpus.sh
@@ -8,7 +8,7 @@ source $VENVPATH/bin/activate
 
 # path to built repository
 # TODO: make this a param in the future???
-INPUTPATH=$_HOME/no-more-secrets
+INPUTPATH=$_HOME/crispy-doom
 
 echo "Running extract_ir.py"
 #docker run --mount type=bind,source=$_HOME,target=$_HOME --platform="linux/amd64" --rm mlgo python3 extract_ir.py \
@@ -17,6 +17,9 @@ echo "Running extract_ir.py"
 #  --input_type=json \
 #  --llvm_objcopy_path=$LLVM_INSTALLDIR/bin/llvm-objcopy \
 #  --output_dir=$CORPUS
+
+rm -rf $CORPUS
+mkdir -p $CORPUS
 
 export PYTHONPATH=$PYTHONPATH:.
 python3 compiler_opt/tools/extract_ir.py \
