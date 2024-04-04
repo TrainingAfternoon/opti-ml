21b77faf3f4e8bcddf06386f2e3d5144f3418694
Sam Keyser <keysers@msoe.edu>
2024-04-04 13:54:40 +0000

    Update collect training data script to use the cpp-perf-benchmark makefile

--- gen-training-data-i2perf.sh
+++ gen-training-data-i2perf.sh
@@ -2,7 +2,10 @@
 
 # generate a whole bunch of different in-lining configs and runtime benchmarks to train the ir2perf model with
 
+source .env
+
 MAX_LOOPS="${1:-37000}"
+CORPUS="${_HOME}/ir2perf-corpus"
 
 modules=(
     "x265" # https://openbenchmarking.org/test/pts/x265
@@ -16,8 +19,8 @@ modules=(
 )
 
 compilers=(
-    "$HOME/compilers/base-clang/bin/clang"
-    "$HOME/compilers/random-clang/bin/clang"
+    "$_HOME/compilers/base-clang/bin/clang"
+    "$_HOME/compilers/random-clang/bin/clang"
 )
 
 heuristic_values=(
@@ -28,6 +31,9 @@ heuristic_values=(
     "1000" # in-line most everything
 )
 num_heuristic_values=${#heuristic_values[@]}
+
+# get into the benchmark directory
+cd $_HOME/cpp-perf-benchmarks-2
 
 LOOPS=0
 while [ $LOOPS -lt $MAX_LOOPS ]
@@ -50,27 +56,24 @@ do
     fi
 
     # set compiler flags
-    export $CC=$_CC
-    export CFLAGS=$_CFLAGS
-    export CXXFLAGS=$CFLAGS
+    #export $CC=$_CC
+    #export CFLAGS=$_CFLAGS
+    #export CXXFLAGS=$CFLAGS
 
     # set result file name
     timestamp=$(date +"%m-%d-%y-%H:%M")
-    export TEST_RESULTS_NAME="${TEST}-${identifier}-${timestamp}"
+    RESULTS_FOLDER="$CORPUS/${TEST}-${identifier}-${timestamp}"
+    mkdir -p $RESULTS_FOLDER
 
-    # force exec to 1 run
-    export FORCE_TIMES_TO_RUN=1
+    # run benchmark
+    make all
+    make report
 
-    # enable perf
-    export LINUX_PERF=1
+    # move artifacts to results file
+    DESTDIR=$RESULTS_FOLDER make package
 
-    # TODO: bind task to 1 cpu
-
-    # TODO: run benchmark
-    # TODO: move code artifacts to results file
-    # TODO: retrieve results from /tmp/perf.data
-
-    # TODO: flush page cache
+    # clean remaining artifacts
+    make clean
 
     # increment loop counter
     LOOPS=$((LOOPS + 1))
